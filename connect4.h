f6db5d5076a72e4342a257d03e3839b21280deff
AnoojBathula <abmw6@mst.edu>
2017-04-14 16:26:51 -0500

    Update connect4.h

--- connect4.h
+++ connect4.h
@@ -4,8 +4,6 @@
 #include "Controller.h"
 using namespace std;
 
-
-
 void print_board(int A[6][7])
 {
 	for(int i=0;i<6;i++)
@@ -20,8 +18,7 @@ void print_board(int A[6][7])
 }
 
 string get_name(string& one)
-{	
-	
+{		
 	command(1);
 	cin >> one;
 	return one;
@@ -32,9 +29,7 @@ string get_name2(string two)
 	command(2);
 	cin >> two;
 	return two;
-
 }
-
 
 int make_move(int A[6][7],string player,int check)
 {
@@ -44,8 +39,7 @@ int make_move(int A[6][7],string player,int check)
 	choice--;
 	
 	if(A[0][choice] !=0)
-	{
-		
+	{		
 		command(3);
 		make_move(A,player,check);
 		return 0;
@@ -84,36 +78,6 @@ int make_move(int A[6][7],string player,int check)
 	return 0;
 }
 
-
-
-/*int make_moveTwo(int A[6][7],int player)
-{
-	int choice;
-	cout << playerTwo << ", please choose a column, numbered from left to right, to place a piece:"<<endl;
-	cin >> choice;
-	choice--;
-	if(A[0][choice] == 1)
-	{
-		cout << "That column is full: please choose another column:"<<endl;
-		make_moveTwo(A,player);
-	}
-	else 
-	{
-		for(int i=5;i>0;i--)
-		{
-			if(A[i][choice]==0)
-			{
-				A[i][choice]=player;
-				cout << "The move was successful!"<<endl;
-				print_board(A);
-				return player;
-			}
-		}
-	}
-	return 0;
-}
-*/
-
 bool win_case(int board[6][7], int char_piece, string player) // standard connect 4 board is 6x7
 {
 	bool winCase = false;
@@ -128,10 +92,7 @@ if(!winCase)
 			if(board[row][col] == char_piece && board[row][col+1] == char_piece && board[row][col+2] == char_piece && board[row][col+3] == char_piece)
 			{
 
-				sendwinner(player);
-
-			
-
+				sendwinner(player);			
 				winCase = true;
 			}
 		}
@@ -146,7 +107,6 @@ if(!winCase)
 			if(board[row][col] == char_piece && board[row+1][col] == char_piece && board[row+2][col] == char_piece && board[row+3][col] == char_piece)
 			{
 				sendwinner(player);
-			
 				winCase = true;
 			}
 		}
@@ -161,7 +121,6 @@ if(!winCase)
 			if(board[row][col] == char_piece && board[row+1][col+1] == char_piece && board[row+2][col+2] == char_piece && board[row+3][col+3] == char_piece)
 			{
 				sendwinner(player);
-				
 				winCase = true;
 			}
 		}
@@ -176,7 +135,6 @@ if(!winCase)
 			if(board[row][col] == char_piece && board[row+1][col-1] == char_piece && board[row+2][col-2] == char_piece && board[row+3][col-3] == char_piece)
 			{
 				sendwinner(player);
-				
 				winCase = true;
 			}
 		}
